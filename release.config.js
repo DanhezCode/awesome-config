f5775f00b411b8a2f86644bcb3f37fcee94dfd6e
DanhezCode <danhezcode@gmail.com>
2026-08-04 05:50:28 -0400

    fix: trying to fix the note generator 4
    - improve dependencies

--- release.config.js
+++ release.config.js
@@ -3,26 +3,31 @@ export default {
   branches: ["main", { name: "beta", prerelease: true, channel: "beta" }],
 
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+      },
+    ],
     [
       "@semantic-release/release-notes-generator",
       {
         preset: "conventionalcommits",
         presetConfig: {
           types: [
-            { type: "feat", section: "🚀 New Features", effect: "bump" },
-            { type: "fix", section: "🐞 Bug Fixes", effect: "bump" },
-            { type: "docs", section: "📚 Documentation Improvements", effect: "bump" },
-            { type: "style", section: "🎨 Code Style & Formatting", effect: "bump" },
-            { type: "refactor", section: "🔧 Code Refactoring", effect: "bump" },
-            { type: "perf", section: "⚡ Performance Improvements", effect: "bump" },
-            { type: "test", section: "🧪 Test Updates", effect: "bump" },
-            { type: "chore", section: "🌀 Miscellaneous", effect: "bump" },
+            { type: "feat", section: "🚀 New Features", effect: "changelog" },
+            { type: "fix", section: "🐞 Bug Fixes", effect: "changelog" },
+            { type: "docs", section: "📚 Documentation Improvements", effect: "changelog" },
+            { type: "style", section: "🎨 Code Style & Formatting", effect: "changelog" },
+            { type: "refactor", section: "🔧 Code Refactoring", effect: "changelog" },
+            { type: "perf", section: "⚡ Performance Improvements", effect: "changelog" },
+            { type: "test", section: "🧪 Test Updates", effect: "changelog" },
+            { type: "chore", section: "🌀 Miscellaneous", effect: "changelog" },
           ],
         },
       },
     ],
-    ["@semantic-release/npm", { npmPublish: true }],
-    ["@semantic-release/github"],
+    "@semantic-release/npm",
+    "@semantic-release/github",
   ],
 };
