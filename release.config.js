957b58b871cce0ce9b6f98140182f7ced1d1a06d
DanhezCode <danhezcode@gmail.com>
2026-08-04 04:55:59 -0400

    fix: trying to fix the note generator 2

--- release.config.js
+++ release.config.js
@@ -8,19 +8,21 @@ export default {
       "@semantic-release/release-notes-generator",
       {
         preset: "conventionalcommits",
-        presetConfig: [
-          { type: "feat", section: "🚀 New Features", hidden: false },
-          { type: "fix", section: "🐞 Bug Fixes", hidden: false },
-          { type: "docs", section: "📚 Documentation Improvements", hidden: false },
-          { type: "style", section: "🎨 Code Style & Formatting", hidden: false },
-          { type: "refactor", section: "🔧 Code Refactoring", hidden: false },
-          { type: "perf", section: "⚡ Performance Improvements", hidden: false },
-          { type: "test", section: "🧪 Test Updates", hidden: false },
-          { type: "chore", section: "🌀 Miscellaneous", hidden: false },
-        ],
+        presetConfig: {
+          types: [
+            { type: "feat", section: "🚀 New Features", hidden: false },
+            { type: "fix", section: "🐞 Bug Fixes", hidden: false },
+            { type: "docs", section: "📚 Documentation Improvements", hidden: false },
+            { type: "style", section: "🎨 Code Style & Formatting", hidden: false },
+            { type: "refactor", section: "🔧 Code Refactoring", hidden: false },
+            { type: "perf", section: "⚡ Performance Improvements", hidden: false },
+            { type: "test", section: "🧪 Test Updates", hidden: false },
+            { type: "chore", section: "🌀 Miscellaneous", hidden: false },
+          ],
+        },
       },
     ],
-    ["@semantic-release/github"],
     ["@semantic-release/npm", { npmPublish: true }],
+    ["@semantic-release/github"],
   ],
 };
