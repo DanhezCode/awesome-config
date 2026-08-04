57cc5e75b8d743b4ce4911e860e306c1a40d34d1
DanhezCode <danhezcode@gmail.com>
2026-08-04 04:11:58 -0400

    feat: add semantic release

---
+++ release.config.js
@@ -0,0 +1,28 @@
+/** @type {import('semantic-release').GlobalConfig} */
+export default {
+  branches: ["main", { name: "beta", prerelease: true, channel: "beta" }],
+
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+        presetConfig: {
+          types: [
+            { type: "feat", section: "🚀 New Features", hidden: false },
+            { type: "fix", section: "🐞 Bug Fixes", hidden: false },
+            { type: "docs", section: "📚 Documentation Improvements", hidden: false },
+            { type: "style", section: "🎨 Code Style & Formatting", hidden: false },
+            { type: "refactor", section: "🔧 Code Refactoring", hidden: false },
+            { type: "perf", section: "⚡ Performance Improvements", hidden: false },
+            { type: "test", section: "🧪 Test Updates", hidden: false },
+            { type: "chore", section: "🌀 Miscellaneous", hidden: false },
+          ],
+        },
+      },
+    ],
+    ["@semantic-release/github"],
+    ["@semantic-release/npm", { npmPublish: true }],
+  ],
+};
